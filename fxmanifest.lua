2a4549fa8cfc2d486135bb1a607ae78254f113d2
Abhishek  Gaikwad <abhishekgaikwad065@gmail.com>
2026-04-17 21:29:21 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 lua54 "yes"
 
 author "Abhishek (Crimvex Studio)"
-description "Crimvex Trucker Job | For support https://discord.gg/7nCnEjG93Q"
+description "Crimvex OX Npc Shops | For support https://discord.gg/7nCnEjG93Q"
 version "1.0.0"
 
 dependencies {
